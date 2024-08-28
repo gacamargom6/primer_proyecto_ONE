80b5a98193d9c2c2afc4c9b60270ff9c83119c29
gacamargom6 <gacamargom@gmail.com>
2024-08-27 21:31:31 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//Definiciones
+let econvertida="enter";
+let iconvertida="imes";
+let aconvertida="ai";
+let oconvertida="ober";
+let uconvertida="ufat"
+
+const  botonEncriptar = document.getElementById('')
