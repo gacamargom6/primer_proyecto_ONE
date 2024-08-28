fc4a4267b2cc3500b84a7988aded53ca77f5228c
gacamargom6 <gacamargom@gmail.com>
2024-08-27 22:42:10 -0500

    Actualizacion1

--- app.js
+++ app.js
@@ -5,4 +5,46 @@ let aconvertida="ai";
 let oconvertida="ober";
 let uconvertida="ufat"
 
-const  botonEncriptar = document.getElementById('')
+//const botonEncriptar = document.getElementById('')
+const textoEntrada = document.getElementById('textoOrigen');
+const textoSalida = document.getElementById('textoDestino');
+//console.log(textoEntrada);
+//console.log(textoSalida);
+
+function botonEncriptar(){
+    const cadenaEncriptada=encriptar(textoEntrada.value);
+    textoSalida.value = cadenaEncriptada;
+    textoEntrada.value="";
+    //console.log(cadenaEncriptada);
+}
+function botonDesencriptar(){
+    const cadenaEncriptada=desencriptar(textoEntrada.value);
+    textoSalida.value = cadenaEncriptada;
+    textoEntrada.value="";
+    //console.log(cadenaEncriptada);
+}
+
+
+
+function encriptar(cadenaEncriptada){
+    let matrizCodigo = [["e",econvertida],["i",iconvertida],["a",aconvertida],["o",oconvertida],["u",uconvertida]];
+    cadenaEncriptada=cadenaEncriptada.toLowerCase();
+    for(let i=0; i<matrizCodigo.length; i++){
+        if(cadenaEncriptada.includes(matrizCodigo[i][0])){
+            cadenaEncriptada = cadenaEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return cadenaEncriptada;
+}
+
+function desencriptar(cadenaEncriptada){
+    let matrizCodigo = [["e",econvertida],["i",iconvertida],["a",aconvertida],["o",oconvertida],["u",uconvertida]];
+    cadenaEncriptada=cadenaEncriptada.toLowerCase();
+    for(let i=0; i<matrizCodigo.length; i++){
+        if(cadenaEncriptada.includes(matrizCodigo[i][1])){
+            cadenaEncriptada = cadenaEncriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+    return cadenaEncriptada;
+}
+
